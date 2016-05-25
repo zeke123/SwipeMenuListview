017ecdd208462981bc2edb12bc71bed57cbca10a
zhou.jian <zekezhoujian@163.com>
2016-05-25 13:04:29 +0800

    周建   首次体验git

---
+++ src/com/example/test/base/BaseActivity.java
@@ -0,0 +1,43 @@
+package com.example.test.base;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * 所有的Activity继承的基类Activity,包含了ActionBar菜单
+ * @author duguang
+ * 博客地址:http://blog.csdn.net/duguang77
+ */
+public abstract class BaseActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setView();
+		initView();
+		setListener();
+	}
+
+	/**
+	 * 设置布局文件
+	 */
+	public abstract void setView();
+
+	/**
+	 * 初始化布局文件中的控件
+	 */
+	public abstract void initView();
+
+	/**
+	 * 设置控件的监听
+	 */
+	public abstract void setListener();
+	
+	
+}
